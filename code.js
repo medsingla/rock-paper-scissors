23206a57e4c81347ee681d15b44bccc134fe97d0
medsingla <medsingla@gmail.com>
2023-07-26 12:47:12 -0400

    Add functionality to play first to five game with computer

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 const options = ["Rock", "Paper", "Scissors"];
-const playerSelection = "Rock";
 
 function getComputerChoice() {
     return options[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection = getComputerChoice()) {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
     if (playerSelection == computerSelection) {
         return `It's a tie! You both picked ${playerSelection}`;
     }
@@ -17,4 +17,23 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
     }
 }
 
-console.log(playRound(playerSelection));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    while (playerScore < 5 && computerScore < 5) {
+        playerSelection = prompt("Choose Rock, Paper, or Scissors");
+        result = playRound(playerSelection);
+        console.log(result);
+        if (result.includes("win")) {
+            playerScore++;
+        }
+        else if (result.includes("lose")) {
+            computerScore++;
+        }
+    }
+    if (playerScore = 5) {
+        return "You win the game!";
+    }
+    return "You lose the game!"
+}
+console.log(game());
